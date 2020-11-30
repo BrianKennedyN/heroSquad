95b1d61bf20f3f38353c88b6cfd5c121c03f0213
BrianKennedyN <amkamjoro@gmail.com>
2020-11-30 22:42:30 +0300

    styled the css

--- src/main/java/models/Squad.java
+++ src/main/java/models/Squad.java
@@ -1,4 +1,46 @@
 package models;
 
+
+import java.util.ArrayList;
+
 public class Squad {
+    private String name;
+    private String cause;
+    private int maxSize;
+    private static ArrayList<Squad>squads = new ArrayList<>();
+    public Squad(String name, String cause, int maxSize) {
+        this.name = name;
+        this.cause = cause;
+        this.maxSize = maxSize;
+        squads.add(this);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCause() {
+        return cause;
+    }
+
+    public void setCause(String cause) {
+        this.cause = cause;
+    }
+
+    public int getMaxSize() {
+        return maxSize;
+    }
+
+    public void setMaxSize(int maxSize) {
+        this.maxSize = maxSize;
+    }
+
+    public static ArrayList<Squad> getSquads() {
+        return squads;
+    }
+
 }
--- src/test/java/models/HeroTest.java
+++ src/test/java/models/HeroTest.java
@@ -1,7 +1,43 @@
 package models;
 
-import static org.junit.jupiter.api.Assertions.*;
+//import static org.junit.jupiter.api.Assertions.*;
 
-class HeroTest {
+import junit.framework.TestCase;
+import org.junit.Test;
+
+import java.lang.reflect.Member;
+
+public class HeroTest extends TestCase {
+    @Test
+    public void testMembers_getCorrectName_String() {
+        Hero hero = new Hero("Brian", 24, "Smart", "Sleeping", "Squad 1");
+        assertEquals("Brian", hero.getName());
+    }
+
+    @Test
+    public void testMembers_getCorrectAge_String() {
+        Hero hero = new Hero("Brian", 24, "Smart", "Sleeping", "Squad 1");
+        assertEquals(24, hero.getAge());
+    }
+
+    @Test
+    public void testMembers_getCorrectStrength_String() {
+        Hero hero = new Hero("Brian", 24, "Smart", "Sleeping", "Squad 1");
+        assertEquals("Smart", hero.getSpecialPowers());
+    }
+
+    @Test
+    public void testMembers_getCorrectWeakness_String() {
+        Hero hero = new Hero("Brian", 24, "Smart", "Sleeping", "Squad 1");
+        assertEquals("Sleeping", hero.getWeakness());
+    }
+
+    @Test
+    public void testMembers_getCorrectSquad_String() {
+        Hero hero = new Hero("Brian", 24, "Smart", "Sleeping", "Squad 1");
+        assertEquals("Squad 1", hero.getSquad());
+    }
+
+
 
 }
